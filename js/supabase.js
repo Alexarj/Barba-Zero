0e6de5449016e20cf89663a0e0b3ad5ba0063d24
Alexarj <fcmggo@gmail.com>
2026-01-30 23:02:33 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_PUBLIC_ANON_KEY";
+const SUPABASE_URL = "https://ujarxofdtmcjulqutpfd.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_ZhOEQgwUX5A6kQDvm0kB1g_houcldK8";
 
 window.supabase = supabase.createClient(
   SUPABASE_URL,
